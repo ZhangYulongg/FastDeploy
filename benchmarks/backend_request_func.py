9e097c587da7faaf4af7513d49d5777d167b73df
ZhangYulongg <1272816783@qq.com>
2025-09-01 19:36:35 +0800

    add CI cases

--- benchmarks/backend_request_func.py
+++ benchmarks/backend_request_func.py
@@ -85,12 +85,30 @@ async def async_request_eb_openai_chat_completions(
         if request_func_input.multi_modal_content:
             content.append(request_func_input.multi_modal_content)
         payload = {
-            "model": request_func_input.model,
-            "messages": request_func_input.history_QA,
+            "model": "null",
+            "messages": [
+                {
+                    "role": "user",
+                    "content": [
+                        {"type": "text", "text": request_func_input.prompt},
+                    ],
+                }
+            ],
+            "top_p": 0.8,
+            "temperature": 0.8,
+            "stop": ["</s>", "<eos>", "<|endoftext|>", "<|im_end|>"],
             "stream": True,
             "stream_options": {
                 "include_usage": True,
                 "continuous_usage_stats": True,
+            },
+            "metadata": {
+                "chat_template_kwargs": {
+                    "options": {
+                        "thinking_mode": "close",
+                    },
+                },
+                "bad_words_token_ids": [101031, 101032, 101027, 101028, 101023, 101024],
             },
         }
         # 超参由yaml传入
--- benchmarks/benchmark_dataset.py
+++ benchmarks/benchmark_dataset.py
@@ -295,7 +295,7 @@ class EBChatDataset(BenchmarkDataset):
             if len(samples) >= num_requests:
                 break
             json_data = entry
-            prompt = entry["messages"][-1].get("content", "")
+            prompt = entry["messages"][-1].get("content", "")[0]["text"]
             history_QA = entry.get("messages", [])
             new_output_len = int(entry.get("max_tokens", 12288))
 

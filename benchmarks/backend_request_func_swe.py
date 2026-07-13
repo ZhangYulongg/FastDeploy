0027103b4ad2fd9a5697954bf100b15d8eee53b9
Zhang Yulong <35552275+ZhangYulongg@users.noreply.github.com>
2026-07-13 20:41:34 +0800

    Update backend_request_func_swe.py

--- benchmarks/backend_request_func_swe.py
+++ benchmarks/backend_request_func_swe.py
@@ -666,6 +666,12 @@ async def async_request_eb_openai_chat_completions(
     if not output.success or output.output_tokens == 0:
         with open("error_output.txt", "a") as f:
             f.write(str(output) + "\n")
+        print(
+            "####error response:",
+            error_text,
+            "####payload:",
+            payload,
+        )
     if pbar:
         pbar.update(1)
     if request_func_input.debug:
